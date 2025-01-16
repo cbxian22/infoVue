cce2c2fcd494f4ef2fc6f1cc1420a7c0c8ef16dc
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-01-16 11:37:00 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,10 +22,10 @@ const imgSize = ref("imgSize");
 }
 .card {
   position: absolute;
-  left: 18%;
-  top: 55%;
-  transform: translateY(-50%);
-  background-color: rgba(255, 255, 255, 0.7);
+  left: 33%;
+  top: 15%;
+  transform: translate(-50%);
+  background-color: rgba(255, 255, 255, 0.9);
   box-sizing: border-box;
   padding: 1.8rem;
   max-width: 30%;
@@ -38,9 +38,9 @@ const imgSize = ref("imgSize");
   .card {
     max-width: 60%;
     padding: 1.2rem;
-    left: 20%;
+    left: 50%;
     top: 55%;
-    transform: translateY(-50%, -50%);
+    transform: translate(-50%, -50%);
   }
 }
 </style>
